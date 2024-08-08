9cb22516c78ff4b13e39bc8e2902a36c5e9802c7
daniel.j.cravens@gmail.com <daniel.j.cravens@gmail.com>
2024-08-08 08:30:03 +0000

    Edited setup_form

--- client_code/setup_form/__init__.py
+++ client_code/setup_form/__init__.py
@@ -16,3 +16,7 @@ class setup_form(setup_formTemplate):
   def process_button_click(self, **event_args):
     """This method is called when the button is clicked"""
     open_form('process_form')
+
+  def file_loader_1_change(self, file, **event_args):
+    """This method is called when a new file is loaded into this FileLoader"""
+    pass
