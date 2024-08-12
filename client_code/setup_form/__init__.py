e6e095645fc39e87f7ca49c95faeb7e3a0ab08de
daniel.j.cravens@gmail.com <daniel.j.cravens@gmail.com>
2024-08-12 03:16:05 +0000

    Edited ServerModule1 and setup_form

--- client_code/setup_form/__init__.py
+++ client_code/setup_form/__init__.py
@@ -6,11 +6,17 @@ import json
 
 
 
+
 class setup_form(setup_formTemplate):
+
+  clientdata = False
+  jsondata = False
+
+
   def __init__(self, **properties):
     # Set Form properties and Data Bindings.
     self.init_components(**properties)
-    # anvil.server.call('say_hello', 'Anvil Developer')
+    self.submit_button.enabled = False
 
     # Any code you write here will run before the form opens.
 
@@ -23,15 +29,19 @@ class setup_form(setup_formTemplate):
     open_form('process_form')
 
   def client_data_change(self, file, **event_args):
-    print(file.name, file)
-    anvil.server.call('SetClientData',file.name, file)
-
-
-  def button_2_click(self, **event_args):
-    text = anvil.server.call('ChangeName', 'clientdanny')
-    self.return_text.text = text
+    self.clientdata = True
+    if(self.jsondata == True):
+      self.submit_button.enabled = True
+    
 
   def json_path_change(self, file, **event_args):
-    data = json.loads(self.json_path.file.get_bytes()) 
-    self.json_display.text = data
-    anvil.media.download(anvil.server.call('MakeXL', data))
+     self.jsondata = True
+     if(self.clientdata == True):
+       self.submit_button.enabled = True
+    
+
+  def submit_button_click(self, **event_args):
+    data = json.loads(self.json_path.file.get_bytes())
+    file = self.client_data.file
+    anvil.media.download(anvil.server.call('Process',data,file))
+    
--- server_code/ServerModule1.py
+++ server_code/ServerModule1.py
@@ -29,9 +29,23 @@ clientDict = ""
 aliasesList = ""
 matterList = ""
 
+
+  
+@anvil.server.callable
+def MakePanda(data):
+  global df 
+  df = pd.json_normalize(data)
+  print(type(df))
+  file_name = 'TimeEntryData.xlsx'
+  datatoexcel = pd.ExcelWriter(file_name)
+  df.to_excel(file_name, columns=['userId','timekeepr','client','matter','task','activity','billable','hoursWorked','hoursBilled','rate','amount','narrative','alias','length','subject','bcc','body', 'cc','date','filename','messageId','recipients','sender'])  # 
+  return anvil.media.from_file(file_name)
+  
+  
 def generateClientAlias(docs):
     llm = ChatOpenAI(temperature=0.3, model_name="gpt-3.5-turbo-16k", api_key=anvil.secrets.get_secret('OPENAI_API_KEY'))
     AliasesString = aliasesList.__str__()
+    print("Alias: ", AliasesString)
 # Define template
 
     template_string = """
@@ -68,66 +82,39 @@ def generateClientAlias(docs):
     print(output)
   
 @anvil.server.callable
-def SetClientData(file_name, file_object):
+def SetClientData(file_object):
   global clientDict
   print("Client_Data_Processing")
   clientDict = pd.read_excel(file_object.get_bytes())
   GetAliasesList()
   GetMatterNumberList()
+  
 
-  generateClientAlias("Alvarez v. Command Security Services")
+ 
 
 def GetAliasesList():
     global clientDict
-    global aliasesList
-    clientData = clientDict
-    aliasesList = clientData.to_dict('list')['Name']
+    global aliasesList  
+
+    aliasesList = clientDict['Name'].to_list()
     aliasesList.insert(0,"None")
-    print(aliasesList)
   
 def GetMatterNumberList():
   global clientDict
   global matterList
-  clientData = clientDict
-  matterList = clientData.to_dict('list')['Client/Matter Number']
+  matterList = clientDict['Client/Matter Number'].to_list()
   matterList.insert(0,"None")
-  print(matterList)
-   
-  # return matterList
-  def generateClientAlias(docs):
+
+  def generateClientAlias():
     llm = ChatOpenAI(temperature=0.3, model_name="gpt-3.5-turbo-16k", api_key=anvil.secrets.get_secret('OPENAI_API_KEY'))
     AliasesString = aliasesList
-# Define template
 
-    template_string = """
-    You are an attorney billing expert. Your job is to infer the client alias from the folowing email: {text} 
-    
-    In most cases, the subject of the email will contain the alias. In those cases you will compare the email subject with the LIST OF APPROVED ALIASES and return the alias FROM THE LIST OF APPROVED ALIASES that best matches the subject line. 
+    for index, row in df.iterrows():
+      print(row['Name'], row['Client/Matter'])
 
-    For example, where the email subject is "topix -- quick questions", you would compare this to the list of approved aliases and infer that Page v. Topix Pharmaceuticals is the best fit for the alias because none of the other aliases contain the work topix.
-    
-    In some cases, the subject will not contain enough information to infer the alias. In those case, you will look at the body of the email for information that matches an alias FROM THE LIST OF APPROVED ALIASES. 
-    Example 1: you may infer the  alias: Page v. Topix Pharmaceuticals where the body of the email refers to a person named Page. 
-    Example 2: you may infer the alias: Aguilera v. Turner Systems, Inc. where the subject of the email refers to Turner.
-
-    IMPORTANT: YOUR RESPONSE SHOULD NOT BE CONVERSATIONAL. YOUR REPSONSE ONLY CONTAIN THE ALIAS FROM THE LIST OF APPROVED ALIASES WITHOUT ANY ADDITIONAL WORDS. 
-
-    INCORRECT response: Based on the information provided in the email, the inferred client alias is "Gonzalez v. DS Electric, Inc."
-    CORRECT response: Gonzalez v. DS Electric, Inc.
-
-    IMPORTANT: IF YOU CANNOT INFER AN ALIAS FROM THE CONTENT PROVIDED, YOUR MUST RESPOND WITH THE SINGLE WORD: None
-
-    INCORRECT response: The inferred client alias from the email is None.
-    CORRECT response: none
-
-    THE LIST OF APPROVED ALIASES FOLLOWS = """ + AliasesString 
-
-
-
-
-    prompt = PromptTemplate.from_template(template_string)
-    
-    chain = load_summarize_chain(llm, chain_type="stuff", prompt=prompt)
-    output_clientmatter = chain.run(docs)
-    output = output_clientmatter.strip()
-    print(output)
+@anvil.server.callable
+def Process(data, file_object):
+  MakePanda(data)
+  SetClientData(file_object)
+  generateClientAlias("Alvarez v. Command Security Services")
+   
